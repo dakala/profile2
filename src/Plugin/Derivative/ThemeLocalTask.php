1110bb5bd701b46b73a87e92f19d10f1a373231c
Deji Akala <dejiakala@gmail.com>
2014-06-12 19:51:12 +0100

    Hide Add <profile_type> tab conditionally.

--- src/Plugin/Derivative/ThemeLocalTask.php
+++ src/Plugin/Derivative/ThemeLocalTask.php
@@ -21,6 +21,7 @@ class ThemeLocalTask extends DerivativeBase {
    * {@inheritdoc}
    */
   public function getDerivativeDefinitions($base_plugin_definition) {
+    $configs = array();
     foreach (\Drupal::configFactory()
                ->listAll('profile2.type.') as $config_name) {
       $config = \Drupal::config($config_name);
@@ -29,33 +30,42 @@ class ThemeLocalTask extends DerivativeBase {
         ->getFieldDefinitions('profile2', $config->get('id')), function ($field_definition) {
         return $field_definition instanceof FieldInstanceConfigInterface;
       });
-
+      // No fields yet.
       if (!$instances) {
         continue;
       }
-
-      // Expose profile types that users may create - either they have 0 of non-multiple or multiple.
-      if ($config->get('multiple') === FALSE) {
-        $user = \Drupal::request()->attributes->get('user');
-        if ($user instanceof UserInterface) {
-          $profiles = entity_load_multiple_by_properties('profile2', array(
-            'uid' => $user->id(),
-            'type' => $config->get('id'),
-          ));
-          if (isset($profiles)) {
-            continue;
+      else {
+        // Expose profile types that users may create - either they have 0 of non-multiple or multiple.
+        if ($config->get('multiple') === FALSE) {
+          $user = \Drupal::request()->attributes->get('user');
+          if ($user instanceof UserInterface) {
+            $profiles = entity_load_multiple_by_properties('profile2', array(
+              'uid' => $user->id(),
+              'type' => $config->get('id'),
+            ));
+            // Single profile, none yet.
+            if (!isset($profiles)) {
+              $configs[] = $config;
+            }
           }
         }
+        else {
+          // Multiple profiles allowed.
+          $configs[] = $config;
+        }
       }
+    }
 
-      $this->derivatives[$config_name] = $base_plugin_definition;
-      $this->derivatives[$config_name]['title'] = \Drupal::translation()
-        ->translate('Add @type profile', array('@type' => Unicode::strtolower($config->get('label'))));
-      $this->derivatives[$config_name]['route_parameters'] = array('type' => $config->get('id'));
+    if (count($configs)) {
+      foreach ($configs as $config) {
+        $this->derivatives[$config->get('id')] = $base_plugin_definition;
+        $this->derivatives[$config->get('id')]['title'] = \Drupal::translation()
+          ->translate('Add @type profile', array('@type' => Unicode::strtolower($config->get('label'))));
+        $this->derivatives[$config->get('id')]['route_parameters'] = array('type' => $config->get('id'));
+      }
     }
 
     return $this->derivatives;
   }
-
 
 }
