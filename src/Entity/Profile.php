9be40883775cc2c965463926430fa2a35372389c
Deji Akala <dejiakala@gmail.com>
2014-07-02 14:57:44 +0100

    Added support for revisions

--- src/Entity/Profile.php
+++ src/Entity/Profile.php
@@ -11,6 +11,7 @@ use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Field\FieldDefinition;
 use Drupal\Core\Entity\ContentEntityBase;
 use Drupal\profile\ProfileInterface;
+use Drupal\user\UserInterface;
 
 /**
  * Defines the profile entity class.
@@ -31,10 +32,12 @@ use Drupal\profile\ProfileInterface;
  *   bundle_entity_type = "profile_type",
  *   admin_permission = "administer profiles",
  *   base_table = "profile",
+ *   revision_table = "profile_revision",
  *   fieldable = TRUE,
  *   translatable = TRUE,
  *   entity_keys = {
  *     "id" = "pid",
+ *     "revision" = "vid",
  *     "bundle" = "type",
  *     "label" = "label",
  *     "uuid" = "uuid"
@@ -45,7 +48,6 @@ use Drupal\profile\ProfileInterface;
  *   },
  * )
  */
-
 class Profile extends ContentEntityBase implements ProfileInterface {
 
   /**
@@ -64,6 +66,12 @@ class Profile extends ContentEntityBase implements ProfileInterface {
       ->setDescription(t('The profile UUID.'))
       ->setReadOnly(TRUE);
 
+    $fields['vid'] = FieldDefinition::create('integer')
+      ->setLabel(t('Revision ID'))
+      ->setDescription(t('The profile revision ID.'))
+      ->setReadOnly(TRUE)
+      ->setSetting('unsigned', TRUE);
+
     $fields['type'] = FieldDefinition::create('entity_reference')
       ->setLabel(t('Profile type'))
       ->setDescription(t('The profile type.'))
@@ -73,20 +81,26 @@ class Profile extends ContentEntityBase implements ProfileInterface {
     $fields['uid'] = FieldDefinition::create('entity_reference')
       ->setLabel(t('User ID'))
       ->setDescription(t('The user ID of the user associated with the profile.'))
+      ->setRevisionable(TRUE)
       ->setSetting('target_type', 'user')
-      ->setSetting('default_value', 0);
+      ->setTranslatable(TRUE);
 
     $fields['langcode'] = FieldDefinition::create('language')
       ->setLabel(t('Language code'))
-      ->setDescription(t('The profile language code.'));
+      ->setDescription(t('The profile language code.'))
+      ->setRevisionable(TRUE);
 
     $fields['created'] = FieldDefinition::create('created')
       ->setLabel(t('Created'))
-      ->setDescription(t('The time that the profile was created.'));
+      ->setDescription(t('The time that the profile was created.'))
+      ->setRevisionable(TRUE)
+      ->setTranslatable(TRUE);
 
     $fields['changed'] = FieldDefinition::create('changed')
       ->setLabel(t('Changed'))
-      ->setDescription(t('The time that the profile was last edited.'));
+      ->setDescription(t('The time that the profile was last edited.'))
+      ->setRevisionable(TRUE)
+      ->setTranslatable(TRUE);
 
     return $fields;
   }
@@ -131,6 +145,21 @@ class Profile extends ContentEntityBase implements ProfileInterface {
   /**
    * {@inheritdoc}
    */
+  public function getOwner() {
+    return $this->get('uid')->entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwner(UserInterface $account) {
+    $this->set('uid', $account->id());
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function getCreatedTime() {
     return $this->get('created')->value;
   }
@@ -148,6 +177,36 @@ class Profile extends ContentEntityBase implements ProfileInterface {
    */
   public function getChangedTime() {
     return $this->get('changed')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRevisionCreationTime() {
+    return $this->get('revision_timestamp')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setRevisionCreationTime($timestamp) {
+    $this->set('revision_timestamp', $timestamp);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRevisionAuthor() {
+    return $this->get('revision_uid')->entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setRevisionAuthorId($uid) {
+    $this->set('revision_uid', $uid);
+    return $this;
   }
 
 }
--- src/ProfileInterface.php
+++ src/ProfileInterface.php
@@ -9,11 +9,38 @@ namespace Drupal\profile;
 
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityChangedInterface;
+use Drupal\user\EntityOwnerInterface;
+use Drupal\user\UserInterface;
 
 /**
  * Provides an interface defining a custom block entity.
  */
-interface ProfileInterface extends ContentEntityInterface, EntityChangedInterface {
+interface ProfileInterface extends ContentEntityInterface, EntityChangedInterface, EntityOwnerInterface {
+
+  public function getType();
+
+  public function setType($type);
 
   public function getOwnerId();
+
+  public function setOwnerId($uid);
+
+  public function getOwner();
+
+  public function setOwner(UserInterface $account);
+
+  public function getCreatedTime();
+
+  public function setCreatedTime($timestamp);
+
+  public function getChangedTime();
+
+  public function getRevisionCreationTime();
+
+  public function setRevisionCreationTime($timestamp);
+
+  public function getRevisionAuthor();
+
+  public function setRevisionAuthorId($uid);
+
 }
