ee71e3a7b9cfaa614f8f87f0201bae3f916e3b6d
Deji Akala <dejiakala@gmail.com>
2014-07-07 17:56:44 +0100

    Added profile list

--- src/Plugin/Derivative/ProfileAddLocalTask.php
+++ src/Plugin/Derivative/ProfileAddLocalTask.php
@@ -57,7 +57,7 @@ class ProfileAddLocalTask extends DeriverBase {
         }
       }
     }
-    
+
     if (count($configs)) {
       foreach ($configs as $config) {
         $id = $config->get('id');
