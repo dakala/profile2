1c0db6665a18f02a9b877995d342b09c17580aea
Deji Akala <deji.akala@gmail.com>
2015-02-26 15:55:54 +0000

    Resolved conflicts

--- src/Tests/ProfileTypeCRUDTest.php
+++ src/Tests/ProfileTypeCRUDTest.php
@@ -8,13 +8,7 @@
 namespace Drupal\profile\Tests;
 
 use Drupal\simpletest\WebTestBase;
-<<<<<<< HEAD
-use Drupal\Component\Utility\String;
 use Drupal\Component\Utility\Unicode;
-||||||| merged common ancestors
-=======
-use Drupal\Component\Utility\Unicode;
->>>>>>> d003659a77f5b53deb489d54b3d20a8286cbffb3
 
 /**
  * Tests basic CRUD functionality of profile types.
@@ -34,16 +28,8 @@ class ProfileTypeCRUDTest extends WebTestBase {
     // Create a new profile type.
     $this->drupalGet('admin/config/people/profiles/types');
     $this->clickLink(t('Add profile type'));
-<<<<<<< HEAD
-    $this->assertUrl('admin/people/profiles/add');
-    $id = Unicode::strtolower($this->randomName());
-||||||| merged common ancestors
-    $this->assertUrl('admin/people/profiles/add');
-    $id = drupal_strtolower($this->randomName());
-=======
     $this->assertUrl('admin/config/people/profiles/types/add');
     $id = Unicode::strtolower($this->randomMachineName());
->>>>>>> d003659a77f5b53deb489d54b3d20a8286cbffb3
     $label = $this->randomString();
     $edit = array(
       'id' => $id,
@@ -68,16 +54,8 @@ class ProfileTypeCRUDTest extends WebTestBase {
     $this->assertRaw(format_string('%label profile type has been updated.', array('%label' => $label)));
 
     // Add a field to the profile type.
-<<<<<<< HEAD
-    $this->drupalGet("admin/people/profiles/manage/$id/fields");
-    $field_name = Unicode::strtolower($this->randomName());
-||||||| merged common ancestors
-    $this->drupalGet("admin/people/profiles/manage/$id/fields");
-    $field_name = drupal_strtolower($this->randomName());
-=======
     $this->drupalGet("admin/config/people/profiles/types/manage/$id/fields/add-field");
     $field_name = Unicode::strtolower($this->randomMachineName());
->>>>>>> d003659a77f5b53deb489d54b3d20a8286cbffb3
     $field_label = $this->randomString();
     $edit = array(
       'new_storage_type' => 'string',
@@ -96,16 +74,8 @@ class ProfileTypeCRUDTest extends WebTestBase {
     $this->assertRaw(format_string('Saved %label configuration.', array('%label' => $field_label)));
 
     // Rename the profile type ID.
-<<<<<<< HEAD
-    $this->drupalGet("admin/people/profiles/manage/$id/edit");
-    $new_id = Unicode::strtolower($this->randomName());
-||||||| merged common ancestors
-    $this->drupalGet("admin/people/profiles/manage/$id/edit");
-    $new_id = drupal_strtolower($this->randomName());
-=======
     $this->drupalGet("admin/config/people/profiles/types/manage/$id");
     $new_id = Unicode::strtolower($this->randomMachineName());
->>>>>>> d003659a77f5b53deb489d54b3d20a8286cbffb3
     $edit = array(
       'id' => $new_id,
     );
@@ -123,7 +93,7 @@ class ProfileTypeCRUDTest extends WebTestBase {
     //   correctly. The pre-existing field does not appear on the Manage
     //   fields page of the renamed bundle. Not even flushing all caches
     //   helps. Can be reproduced manually.
-    //$this->assertText(String::checkPlain($field_label));
+    //$this->assertText(check_plain($field_label));
   }
 
 }
