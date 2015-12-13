066fbe3c98f38ab8f3e868f70d1257b0dd55ec70
Jibran Ijaz <jibran@1198144.no-reply.drupal.org>
2015-12-13 21:49:51 +0500

    Clear up the redundant data and views plugins.

--- src/ProfileViewsData.php
+++ src/ProfileViewsData.php
@@ -8,6 +8,7 @@
 namespace Drupal\profile;
 
 use Drupal\views\EntityViewsData;
+
 /**
  * Provides the views data for the node entity type.
  */
@@ -18,38 +19,6 @@ class ProfileViewsData extends EntityViewsData {
    */
   public function getViewsData() {
     $data = parent::getViewsData();
-
-    $data['profile']['label'] = [
-      'title' => t('Label'),
-      'help' => t('The label of the profile item.'),
-      'field' => [
-        'id' => 'profile_label',
-      ],
-    ];
-
-    $data['profile']['view_profile'] = [
-      'field' => [
-        'title' => t('Link to profile'),
-        'help' => t('Provide a simple link to the profile.'),
-        'id' => 'profile_link',
-      ],
-    ];
-
-    $data['profile']['edit_profile'] = [
-      'field' => [
-        'title' => t('Link to edit profile'),
-        'help' => t('Provide a simple link to edit the profile.'),
-        'id' => 'profile_link_edit',
-      ],
-    ];
-
-    $data['profile']['delete_profile'] = [
-      'field' => [
-        'title' => t('Link to delete profile'),
-        'help' => t('Provide a simple link to delete the profile.'),
-        'id' => 'profile_link_delete',
-      ],
-    ];
 
     $data['profile']['profile_bulk_form'] = [
       'title' => t('Profile operations bulk form'),
